d7d38ce179c5f86857fb259bc87dd399d81daa2d
Carlos Camacho <ccamachog@gmail.com>
2019-01-27 11:51:59 +0100

    added test controller

--- src/main/java/com/camcor/payments/DemoApplication.java
+++ src/main/java/com/camcor/payments/DemoApplication.java
@@ -2,12 +2,20 @@ package com.camcor.payments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	@GetMapping("/test")
+	public String test(){
+		return "hello world";
 	}
 
 }
